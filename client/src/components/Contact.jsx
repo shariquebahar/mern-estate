fa0056673d98b7bf6524de4196f26700e86cd4d3
sharique bahar <baharsharique@gmail.com>
2024-08-01 17:52:56 +0530

    creating contacting landlord functionality to the listing page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export default function Contact({ listing }) {
   const [landlord, setLandlord] = useState(null);
   const [message, setMessage] = useState("");
+
   const onChange = (e) => {
     setMessage(e.target.value);
   };
